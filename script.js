f35b9159bfba0c277fc4719a46d1cdfc8aaabe52
iswarya-git <iswaryasbsi@gmail.com>
2025-05-01 21:32:28 +0530

    Add files via upload
    # Update styles

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ const totalDisplay = document.getElementById('total-amount');
 
 options.forEach(option => {
   option.addEventListener('click', () => {
-    // Remove "selected" from all options
     options.forEach(opt => opt.classList.remove('selected'));
-    // Add "selected" to the clicked option
     option.classList.add('selected');
-
-    // Update total price
     const price = option.getAttribute('data-price');
     totalDisplay.textContent = `DKK ${parseFloat(price).toFixed(2)}`;
   });
